9532a457aa05fcfca4fe5b38fc006013125a79ce
Cjmin-korean <jmin9003@naver.com>
2022-03-24 22:49:43 +0900

    int String List Dto

--- basic/basic/src/main/java/com/spring/basic/dao/BoardDAO.java
+++ basic/basic/src/main/java/com/spring/basic/dao/BoardDAO.java
@@ -7,15 +7,37 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import com.spring.basic.model.BasicDto;
 import com.spring.basic.model.Employee;
 
 @Repository
 public class BoardDAO {
-    @Inject
-   SqlSession sqlSession;
+	@Inject
+	SqlSession sqlSession;
 
-   public List<Employee> boardList() {
-    System.out.println("run BoardDAO boardList()");
-    return sqlSession.selectList("BoardDAO.boardList");
- }
+	public List<Employee> boardList() {
+		System.out.println("run BoardDAO boardList()");
+		return sqlSession.selectList("BoardDAO.boardList");
+	}
+
+	public int basicCount() {
+		// mapper.xml || namespace.id
+		System.out.println("run BoardDAO basiccount() go to mapper");
+		return sqlSession.selectOne("BoardDAO.basicCount");
+	}
+
+	public String basicName() {
+		// mapper.xml || namespace.id
+		System.out.println("run BoardDAO basicname() go to mapper");
+		return sqlSession.selectOne("BoardDAO.basicName");
+	}
+
+	public BasicDto basicData() {
+		return sqlSession.selectOne("BoardDAO.basicName");
+	}
+
+	public List<BasicDto> basicList() {
+		return sqlSession.selectList("BoardDAO.basicList");
+	}
+
 }
